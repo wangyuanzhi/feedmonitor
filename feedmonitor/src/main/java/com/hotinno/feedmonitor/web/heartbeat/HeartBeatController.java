39bf5e6609aabf8c3e7f41c391dae8a642eec12e
gamenow <gamenow@gmail.com>
2013-01-01 16:47:40 +0800

    Some minor changes for HeartBeatController.

--- feedmonitor/src/main/java/com/hotinno/feedmonitor/web/heartbeat/HeartBeatController.java
+++ feedmonitor/src/main/java/com/hotinno/feedmonitor/web/heartbeat/HeartBeatController.java
@@ -3,14 +3,21 @@ package com.hotinno.feedmonitor.web.heartbeat;
 import java.util.Date;
 import java.util.List;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.hotinno.feedmonitor.dao.feed.FeedDao;
 import com.hotinno.feedmonitor.dao.heartbeat.HeartBeat;
 import com.hotinno.feedmonitor.dao.heartbeat.HeartBeatDao;
 import com.hotinno.feedmonitor.task.BtSeedsMonitor;
@@ -18,21 +25,27 @@ import com.hotinno.feedmonitor.util.WebUtil;
 
 @Controller
 public class HeartBeatController {
+	private static Log log = LogFactory.getLog(HeartBeatController.class);
+
 	@Autowired
 	private HeartBeatDao heartBeatDao;
+
+	@Autowired
+	private FeedDao feedDao;
 
 	@Autowired
 	private BtSeedsMonitor btSeedsMonitor;
 
 	@RequestMapping(value = "/heartbeat")
-	public ModelAndView guestbook(HttpServletRequest request) {
+	public ModelAndView hearbeat(final HttpServletRequest request) {
 		if ("POST".equals(request.getMethod())) {
 			// Handle a new guest (if any):
 			String token = request.getParameter("token");
 			if ("3405638492761444561L".equals(token)) {
 				String ip = WebUtil.getClientIpAddr(request);
 				List<HeartBeat> heartBeats = heartBeatDao.getLastHeartBearts(1);
-				if (heartBeats.size() > 0 && StringUtils.equals(ip, heartBeats.get(0).getIp())) {
+				if (heartBeats.size() > 0
+						&& StringUtils.equals(ip, heartBeats.get(0).getIp())) {
 					heartBeats.get(0).setLastBeat(null);
 					heartBeatDao.merge(heartBeats.get(0));
 				} else {
@@ -40,7 +53,21 @@ public class HeartBeatController {
 				}
 
 				// Notify seeds monitor that transmission is alive.
-				btSeedsMonitor.checkSeeds();
+				new Thread(new Runnable() {
+					@Override
+					public void run() {
+						log.info("Checking seeds...");
+						btSeedsMonitor.checkSeeds();
+					}
+				}).start();
+
+				// Kick-off a thread to check feed monitor
+				new Thread(new Runnable() {
+					@Override
+					public void run() {
+						checkFeedMonitor(request);
+					}
+				}).start();
 			}
 		}
 
@@ -55,5 +82,34 @@ public class HeartBeatController {
 
 		// Prepare the result view (heartbeat.jsp):
 		return new ModelAndView("heartbeat", "heatbeats", heatbeats);
+	}
+
+	public void checkFeedMonitor(HttpServletRequest request) {
+		Date lastFetchTime = feedDao.getLastFetchTime();
+
+		if (System.currentTimeMillis() - lastFetchTime.getTime() > 20 * 60 * 1000) {
+			log.warn("It is more than 20 minutes that did not check feeds.");
+
+			ApplicationContext applicationContext = WebApplicationContextUtils
+					.getWebApplicationContext(request.getSession()
+							.getServletContext());
+
+			log.warn("Class of appContext is: "
+					+ applicationContext.getClass().getName());
+
+			ApplicationContext parentContext = applicationContext.getParent();
+
+			if (parentContext != null
+					&& parentContext instanceof ConfigurableApplicationContext) {
+				log.warn("Refresh appContext...");
+				((ConfigurableApplicationContext) parentContext).refresh();
+			}
+
+			log.warn("Done.");
+		}
+	}
+
+	private void refreshServlet() throws ServletException {
+		// TODO
 	}
 }
