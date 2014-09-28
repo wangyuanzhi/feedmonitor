33ffc68202ca2f6707788d7fc82c239d123e3996
Game Now <gamenow@gmail.com>
2014-09-28 10:51:14 +0800

    Remove some charactors

--- feedmonitor-webapp/src/main/java/com/hotinno/feedmonitor/util/WebUtil.java
+++ feedmonitor-webapp/src/main/java/com/hotinno/feedmonitor/util/WebUtil.java
@@ -6,8 +6,6 @@ public class WebUtil {
 
 	public static String getClientIpAddr(HttpServletRequest request) {
 		String ip = request.getHeader("x-forwarded-for");
-		// 判断是否为反向代理,多级反向代理的话，X-Forwarded-For的值并不止一个，而是一串IP值，究竟哪个才是真正的用户端的真实IP呢？
-		// 是取X-Forwarded-For中第一个非unknown的有效IP字符串
 		if (ip != null && !"".equals(ip) && !"unknown".equalsIgnoreCase(ip)) {
 			String[] tempArray = ip.split(",");
 			for (int i = 0; i < tempArray.length; i++) {
